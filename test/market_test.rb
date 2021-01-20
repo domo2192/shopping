400fc3b56a5e5176d81619336d07203f5e052fd3
Dominic Padula <domopadula2192@gmail.com>
2021-01-20 09:33:45 -0700

    potential rev methods and tests

--- test/market_test.rb
+++ test/market_test.rb
@@ -50,4 +50,18 @@ class MarketTest < Minitest::Test
     assert_equal [@vendor2], @market.vendors_that_sell(@item4)
 
   end
+
+  def test_potential_revenue_for_vendors
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    assert_equal 29.75, @vendor1.potential_revenue
+    assert_equal 345.00, @vendor2.potential_revenue
+    assert_equal 48.75, @vendor3.potential_revenue
+  end
 end
