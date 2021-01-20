6986366972b37344763f6f4b2602f2a07cf225b4
Dominic Padula <domopadula2192@gmail.com>
2021-01-20 09:18:18 -0700

    Test for multiple items and amounts

--- test/vender_test.rb
+++ test/vender_test.rb
@@ -21,5 +21,16 @@ class VendorTest < Minitest::Test
     assert_equal 0, @vendor.check_stock(@item1)
     @vendor.stock(@item1, 30)
     assert_equal 30, @vendor.check_stock(@item1)
+    expected = {@item1 => 30}
+    assert_equal expected, @vendor.inventory
+  end
+
+  def test_we_can_add_multiple_items_and_amounts
+    @vendor.stock(@item1, 30)
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+    expected = {@item1 => 55,
+                @item2 => 12}
+    assert_equal expected, @vendor.inventory
   end
 end
