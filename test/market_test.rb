4e8c386d4614105f9b4db760e4ec098f7df9bcf6
Dominic Padula <domopadula2192@gmail.com>
2021-01-20 09:29:34 -0700

    Vendors that sell items test

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,7 +9,7 @@ class MarketTest < Minitest::Test
   def setup
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
     @vendor2 = Vendor.new("Ba-Nom-a-Nom")
@@ -34,6 +34,19 @@ class MarketTest < Minitest::Test
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
-    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names 
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
+  end
+
+  def test_vendors_that_sell
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+
   end
 end
