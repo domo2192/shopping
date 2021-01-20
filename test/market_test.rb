a54ffbf6700aadaf2d596a7d6f054a7bc11a8464
Dominic Padula <domopadula2192@gmail.com>
2021-01-20 09:21:28 -0700

    existance and attributes for market

---
+++ test/market_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of Market, @market
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+end
